2d489515dc795d19450b15161a9f6d922d53e122
Khozhiakbar <kh.spb@mail.ru>
2024-04-26 09:54:44 +0500

    committed

---
+++ app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+import "./globals.css";
+import NavBar from "./components/nav/NavBar";
+import Footer from "./components/footer/Footer";
+import CartProvider from "@/providers/CartProvider";
+import { Toaster } from "react-hot-toast";
+
+
+
+const poppins = Poppins({ subsets: ["latin"],weight:
+['400','700'] });
+
+export const metadata: Metadata = {
+  title: "e-Shop",
+  description: "Ecommerce app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} text-slate-700`}>
+        <Toaster
+      toastOptions={{
+        style:{
+          background: 'rgb(51 66 85 )',
+          color : '#fff',
+        }
+      }}
+        />
+        <CartProvider>
+        <div className="flex flex-col min-h-screen">
+        <NavBar/>
+        <main className="flex-grow">
+        {children}
+        </main>
+        <Footer/>
+        </div>
+        </CartProvider>
+        </body>
+    </html>
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig}*/
+const nextConfig = {
+    
+   images:{
+    unoptimized:true,
+    domains:
+    [
+        'm.media-amazon.com',"lh3.googleusercontent.com"
+    ],
+   },
+   
+};
+
+export default nextConfig
